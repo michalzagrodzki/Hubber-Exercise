99dd4c9e5300b22ad2f86635b72e1634f7dee1cb
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-26 20:31:29 +0200

    Scaffold Angular with yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hubberApplicationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hubberApplicationApp
+ */
+angular.module('hubberApplicationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
