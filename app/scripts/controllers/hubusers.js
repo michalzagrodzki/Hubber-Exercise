332795a20b984f90b0cd2aece2e7cda952d4dc75
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-27 18:23:14 +0200

    Add controller for redirecting to single user View.

--- app/scripts/controllers/hubusers.js
+++ app/scripts/controllers/hubusers.js
@@ -9,8 +9,14 @@
  */
 var hubuserControllers = angular.module('hubuserControllers', []);
 
-// List Users of Github ( QUERY )
+// List Users of Github ( QUERY ) - List
 hubuserControllers.controller('ListHubusersCtrl', ['$scope', 'Hubuser',
   function($scope, Hubuser){
     $scope.hubusers = Hubuser.query();
 }]);
+
+// Single User of Github ( GET ) - View
+hubuserControllers.controller('ViewHubusersCtrl', ['$scope', '$routeParams', 'Hubuser',
+  function($scope, $routeParams, Hubuser){
+    $scope.hubuser = Hubuser.get({ login: $routeParams.login });
+  }]);
