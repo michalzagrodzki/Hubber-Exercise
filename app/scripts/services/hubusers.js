58f689053e7b093b92f05342ca91e576edc20092
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-30 21:33:41 +0200

    Correct missing $stateParam declaration in services.

--- app/scripts/services/hubusers.js
+++ app/scripts/services/hubusers.js
@@ -11,7 +11,7 @@
 // factory for calling data from github API
 var hubusersServices = angular.module('hubusersServices', []);
 
-hubusersServices.factory('hubusersFactory', ['$http', function($http){
+hubusersServices.factory('hubusersFactory', ['$stateParams', '$http', function($stateParams, $http){
 
   var baseUrl = "https://api.github.com/users";
   var hubusersFactory = {};
@@ -22,8 +22,8 @@ hubusersServices.factory('hubusersFactory', ['$http', function($http){
   };
 
   // GET single github user
-  hubusersFactory.getHubUser = function(stateParams) {
-    return $http.get(baseUrl + '/' + stateParams.login);
+  hubusersFactory.getHubUser = function($stateParams) {
+    return $http.get(baseUrl + '/' + $stateParams.login);
   };
 
   return hubusersFactory;
